1e446fc60f76294527cf493df2af8a9470eccbb4
Vadim Rybalko <vadim@bionicman.name>
2024-09-24 23:35:21 +0000

    Import version 3.2 from upstream

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.2-RC3';
+$wp_version = '3.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 18226;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '342-20110610';
+$tinymce_version = '342-20110630';
 
 /**
  * Holds the cache manifest version
