3a2bd9b8af2159d52e6bfa8e461ac615c928fa88
Vadim Rybalko <vadim@bionicman.name>
2024-09-25 20:05:14 +0000

    Import version 4.8-RC2 from upstream

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-RC1';
+$wp_version = '4.8-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4602-20170524';
+$tinymce_version = '4603-20170530';
 
 /**
  * Holds the required PHP version
